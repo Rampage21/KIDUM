219244241741cce3b2101133b326c9edfb6569d3
Rampage <Rampage@DESKTOP-EKMG9S4.home>
2021-04-12 10:34:43 +0200

    new

--- KIDUML5.m
+++ KIDUML5.m
@@ -7,9 +7,9 @@ clear all; clc;
 syms a1 a2 d3 real
 % zadanie wsporzednych środków masy członów względem ich lokalnych
 % układów wspołrzednych
-wsp=[-a1/2,0,0,1;-a2/2,0,0,1;0,0,-d3/2,1;0,0,0,1];
+wsp=[0,0,0,1;0,-d2/2,0,1;-a3/2,0,0,1;0,0,0,1;0,0,-d5/2,1];
 % przyjecie modelu rozkladu masy czlonu
-R=[1,1,1,0];
+R=[0,1,1,0,1];
 % w manipulatorze SCARA pierwszy czlon jest pretem (nie cylindryczna kolumna)
 J1=0;
 % wyznaczenie symboliczne macierzy pseudoinercji J i oznaczenie mas członów
@@ -32,5 +32,11 @@ gg=[0,0,-9.81,0];
 F=fun_F(J,ms,vz,az,gg,gp,zmie,wsp);
 
 FSS1=subs(F(1),{'m1','m2','m3','m4',a1,a2},{4,2,1,1,0.7,0.5});
-FS1=double(subs(FSS1,{th1,th2,d3,th4,vz1,vz2,vz3,vz4,az1,az2,az3,az4},{q1,q2,q3,q4,v1,
-v2,v3,v4,ap1,ap2,ap3,ap4}));
+FS1=double(subs(FSS1,{th1,th2,d3,th4,vz1,vz2,vz3,vz4,az1,az2,az3,az4},{q1,q2,q3,q4,v1,v2,v3,v4,ap1,ap2,ap3,ap4}));
+
+
+A1=mA(th1,0,0,0);
+A2=mA(0,d2,0,-pi/2);
+A3=mA(th3,0,a3,0);
+A4=mA(th4,0,0,-pi/2);
+A5=mA(pi/2,d5,0,0);
